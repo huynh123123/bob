206905e2e282b0d763ef3ba4cee52f39d5a54110
Galaxyowo <133757941+Galaxyowo@users.noreply.github.com>
2023-09-06 02:08:21 +0900

    b

--- layouts/header.php
+++ layouts/header.php
@@ -10,7 +10,6 @@ require_once '.\1.Database\connection.php';
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
     <link rel="stylesheet" href="./assets/css/gallery-grid.css">
